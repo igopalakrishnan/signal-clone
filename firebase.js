e6d081481167cd9ae10cc6cc5076b9f354cac889
igopalakrishnan <kingkrishna809@gmail.com>
2022-09-12 13:41:37 +0530

    setup auth and db

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBhfLXBvHiQm_Z-HyEsYlL2o43N8hwuUq8",
     authDomain: "signal-clone-7dd70.firebaseapp.com",
@@ -5,4 +9,17 @@ const firebaseConfig = {
     storageBucket: "signal-clone-7dd70.appspot.com",
     messagingSenderId: "771932868528",
     appId: "1:771932868528:web:d951945c9fd8da150d1075"
-  };
+};
+
+let app;
+
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
