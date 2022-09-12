b4fd4e73174208219cc5479fc73737505ce99989
igopalakrishnan <kingkrishna809@gmail.com>
2022-09-12 13:15:56 +0530

    LoginScreen and RegisterScreen

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhfLXBvHiQm_Z-HyEsYlL2o43N8hwuUq8",
+    authDomain: "signal-clone-7dd70.firebaseapp.com",
+    projectId: "signal-clone-7dd70",
+    storageBucket: "signal-clone-7dd70.appspot.com",
+    messagingSenderId: "771932868528",
+    appId: "1:771932868528:web:d951945c9fd8da150d1075"
+  };
